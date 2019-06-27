646e7059b97f8a6ecab334ff6e107e94b915e840
BackChul <qorcjf7409@naver.com>
2019-06-27 17:20:18 +0900

    no message

--- Assets/Scripts/QuizUI.cs
+++ Assets/Scripts/QuizUI.cs
@@ -6,25 +6,25 @@ using UnityEngine.UI;
 
 public class QuizUI : MonoBehaviour
 {
-    public int quizScore = 0;
-    public GameObject quizImage;
-
+    private int quizScore = 0;
+    private int Round = 1;
+    public int QuizeCount;
+    private Image image;
     // Start is called before the first frame update
     void Start()
     {
-        
+        image = transform.Find("QuizImage").gameObject.GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // 만약 정답이라면 스코어 ++ 오답이라면 무시
-
-        if()
     }
 
-    public void switchquiz()
+    public void PlusScore()
     {
-
+        ++quizScore;
     }
+    
 }
