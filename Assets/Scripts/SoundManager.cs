28faf4da77fba5e9cc1b74fbf48289535a1b74a6
BackChul <qorcjf7409@naver.com>
2019-06-28 00:02:36 +0900

    로고 사운드 추가

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager _instance = null;
+    public static SoundManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType(typeof(SoundManager)) as SoundManager;
+                if (_instance == null)
+                {
+                    Debug.LogError("Error SoundManager");
+                }
+            }
+            return _instance;
+        }
+    }
+
+    public AudioSource musicSource;
+    public AudioSource effectSource;
+    public AudioSource LogoSource;
+
+    private void Awake()
+    {
+        if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        musicSource.clip = clip;
+        musicSource.Play();
+    }
+
+    public void PlayEffect(AudioClip clip)
+    {
+        effectSource.clip = clip;
+        effectSource.Play();
+    }
+}
