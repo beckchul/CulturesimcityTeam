e9aaf3bea78aeef3a54036f5e6a67e95ba48a6f5
zoekim7 <52200797+zoekim7@users.noreply.github.com>
2019-06-27 16:40:45 +0900

    Quiz UI 생성

---
+++ Assets/Scripts/QuizUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class QuizUI : MonoBehaviour
+{
+    public int quizScore = 0;
+    public GameObject quizImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 만약 정답이라면 스코어 ++ 오답이라면 무시
+
+        if()
+    }
+
+    public void switchquiz()
+    {
+
+    }
+}
